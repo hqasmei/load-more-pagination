6f5123d3dbf339c28137d35e32e96fd6c013395d
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-02-18 20:00:22 -0800

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="mx-auto min-h-screen items-center justify-center ">
+      <div className="p-56">
+        <h1 className="text-center text-3xl font-bold">Load More Pagination</h1>
+      </div>
+    </main>
+  )
+}
